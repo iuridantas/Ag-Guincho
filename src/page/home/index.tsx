daa0c8a5bc535ccc09053ff15e6ee64ef2bc6332
iuridantas <uri.dantas@hotmail.com>
2023-09-26 20:07:58 -0300

    feat: created button wpp and phone

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -2,13 +2,42 @@ import { SessionAboutUs } from '../../components/session/sessionAboutUs';
 import { SessionAssistance } from '../../components/session/sessionAssistance';
 import { SessionContact } from '../../components/session/sessionContact';
 import { SessionInitial } from '../../components/session/sessionInitial';
-import { Container } from './style';
+import { Container, Buttons } from './style';
+import { FaWhatsappSquare, FaPhoneAlt } from 'react-icons/fa';
 
 export function Home() {
-  return <Container aria-label="Página inicial">
-    <SessionInitial/>
-    <SessionAssistance/>
-    <SessionAboutUs/>
-    <SessionContact/>
-  </Container>;
+  const phoneNumber = '5577988461478';
+
+  const handlePhoneButtonClick = () => {
+    window.location.href = `tel:${phoneNumber}`;
+  };
+
+  return (
+    <Container aria-label="Página inicial">
+      <Buttons>
+        <a
+          href="https://api.whatsapp.com/send?phone=5577988461478."
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Número do WhatsApp de AG Guincho"
+          title="Número do WhatsApp de AG Guincho"
+        >
+          <FaWhatsappSquare
+            className="whatsApp"
+            alt="Ícone representando o WhatsApp de AG Guincho"
+          />
+        </a>
+        <a className="phoneButton" onClick={handlePhoneButtonClick}>
+          <FaPhoneAlt
+            className="phone"
+            alt="Ícone representando o telefone de AG Guincho"
+          />
+        </a>
+      </Buttons>
+      <SessionInitial />
+      <SessionAssistance />
+      <SessionAboutUs />
+      <SessionContact />
+    </Container>
+  );
 }
--- src/page/home/style.ts
+++ src/page/home/style.ts
@@ -3,4 +3,112 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media only screen and (max-width: 600px) {
+    padding-bottom: 55px;
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 1024px) {
+    padding-bottom: 100px;
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: fixed;
+  z-index: 1;
+  background-color: #ffffff;
+  color: green;
+  border-radius: 8px;
+  margin: 5px;
+  top: 80%;
+  right: 5%;
+
+  a + a {
+    border-left: 1px solid #000;
+    padding-left: 10px;
+  }
+
+  a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    margin: 0 5px;
+    padding: 5px;
+  }
+
+  .phoneButton {
+    display: none;
+  }
+
+  .whatsApp {
+    font-size: 80px;
+    color: green;
+  }
+
+  .phone {
+    font-size: 80px;
+    color: green;
+  }
+
+  @media only screen and (max-width: 600px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px;
+    border-radius: 30px;
+    border: 1px solid black;
+    top: 90%;
+    right: 33%;
+
+    .phoneButton {
+      display: flex;
+    }
+
+    .whatsApp,
+    .phone {
+      font-size: 35px;
+    }
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 900px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px;
+    border-radius: 30px;
+    border: 1px solid black;
+    top: 88%;
+    right: 35%;
+
+    .phoneButton {
+      display: flex;
+    }
+
+    .whatsApp,
+    .phone {
+      font-size: 80px;
+    }
+  }
+
+  @media only screen and (min-width: 901px) and (max-width: 1024px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px;
+    border-radius: 30px;
+    border: 1px solid black;
+    top: 91%;
+    right: 38%;
+
+    .phoneButton {
+      display: flex;
+    }
+
+    .whatsApp,
+    .phone {
+      font-size: 80px;
+    }
+  }
 `;
